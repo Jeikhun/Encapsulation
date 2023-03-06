1c4895a941be0de5fe43620efec44e28817a456e
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2023-03-06 18:29:31 +0400

    Update

--- Encapsulation/User.cs
+++ Encapsulation/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,26 @@ namespace Encapsulation
 {
     internal class User
     {
-        
+        private string _password;
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string Password {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (SetPass(value) == true)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Shifre duzgun deyil");
+                }
+
+            }
+        }
         public User(string username, string password)
         {
 
@@ -39,9 +56,29 @@ namespace Encapsulation
             }
             else
             {
-                
                 return true;
-
+            }
+        }
+        public bool SetPass(string pass)
+        {
+            if (pass.Length < 8)
+            {
+                Console.WriteLine("Password minimal 8 simvoldan ibaret olmalidir");
+                return false;
+            }
+            else if (!pass.Any(char.IsDigit))
+            {
+                Console.WriteLine("Password-da minimal 1 reqem olmalidir");
+                return false;
+            }
+            else if (!pass.Any(char.IsUpper))
+            {
+                Console.WriteLine("Password-da minimal 1 boyuk herf olmalidir");
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
     }
